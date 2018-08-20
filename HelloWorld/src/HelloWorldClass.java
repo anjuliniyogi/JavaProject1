0ef7ad61e16f9108d46e1d13db58ae6664161690
APCS2nd <APCS2nd@MHS-Math2-21>
2018-08-20 09:49:03 -0700

    niyogi added

---
+++ HelloWorld/src/HelloWorldClass.java
@@ -0,0 +1,10 @@
+// java program prints to console 
+// @anjuliniyogi fall2018
+
+public class HelloWorldClass {
+	public static void main(String[] args) {
+		  
+		System.out.println("Hello World" + " Anjuli Niyogi");
+	}
+
+}
