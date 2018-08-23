354b93e2249e89eae7105162a7448c3e9cbf6c13
anjuli <anjuli@anjulis-air>
2018-08-22 20:44:22 -0700

    initial 4

--- HelloWorld/src/HelloWorldClass.java
+++ HelloWorld/src/HelloWorldClass.java
@@ -4,7 +4,7 @@
 public class HelloWorldClass {
 	public static void main(String[] args) {
 		  
-		System.out.println("Hello World" + " Anjuli Niyogi, Sophomore");
+		System.out.println("Hello World" + " Anjuli Niyogi, ");
 	}
 
 }
