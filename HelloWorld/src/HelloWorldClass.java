c1602b0b1fadbd3e9d49bc8966cd81738b2e866c
anjuli <anjuli@anjulis-air>
2018-08-22 20:42:10 -0700

    grade added

--- HelloWorld/src/HelloWorldClass.java
+++ HelloWorld/src/HelloWorldClass.java
@@ -4,7 +4,7 @@
 public class HelloWorldClass {
 	public static void main(String[] args) {
 		  
-		System.out.println("Hello World" + " Anjuli I. Niyogi");
+		System.out.println("Hello World" + " Anjuli Niyogi, Sophomore");
 	}
 
 }
